32bc5d3fe3095e2792af11458db697d0a1ef04f3
tsaito@moet <tsaito@moet>
2015-10-20 12:50:42 +0900

    modified treat_result script

--- cui/scripts/treat_results.py
+++ cui/scripts/treat_results.py
@@ -387,7 +387,7 @@ def aggregate_significant_output(result_dir_path):
                 data = np.genfromtxt(_target_file,
                                      delimiter=',',
                                      dtype=dt,
-                                     skiprows=0)
+                                     skiprows=1)
                 if data.ndim == 0:
                     data = np.atleast_1d(data)
                 for _d in data:
@@ -397,8 +397,11 @@ def aggregate_significant_output(result_dir_path):
                         npv_result[combination_key] = npv
                     if not fuel_cost_result.has_key(combination_key):
                         fuel_cost_result[combination_key] = fuel_cost
-            maximum_key = max(npv_result.items(), key=itemgetter(1))[0]
-            result_dict[target_dir] = [maximum_key, npv_result[maximum_key], len(npv_result.keys()) / float(whole_design_nums)]
+            try:
+                maximum_key = max(npv_result.items(), key=itemgetter(1))[0]
+                result_dict[target_dir] = [maximum_key, npv_result[maximum_key], len(npv_result.keys()) / float(whole_design_nums)]
+            except:
+                result_dict[target_dir] = ['--------', 0, 0.0]
                     
             # output_csv
             output_dir_path = "%s/%s/aggregated_results" % (result_dir_path, target_dir)
