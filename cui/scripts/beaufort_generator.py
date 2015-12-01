8d08cc0809a2be20f0c53321ac6a27f1c70de6e2
tstomoki <tstomoki4@gmail.com>
2015-12-01 19:46:45 +0900

    added minute beaufort scale

--- cui/scripts/beaufort_generator.py
+++ cui/scripts/beaufort_generator.py
@@ -61,7 +61,7 @@ def designated_kinds(args):
     matplotlib.pyplot.clf()    
     
 def all_kinds():
-    alpha_a  = range(1,10)
+    alpha_a  = np.arange(1,10, 0.5)
     output_dir_path = "../results/beauforts"
     initializeDirHierarchy(output_dir_path)
     result = {}
@@ -75,7 +75,7 @@ def all_kinds():
         x_data = bfs.transpose()[0]
         y_data = bfs.transpose()[1]
         title  = "incidence rate of Beaufort".title()
-        output_file_path = "%s/alpha_%d_beta_%d.png" % (output_dir_path, alpha, beta)
+        output_file_path = "%s/alpha_%1.1lf_beta_%1.1lf.png" % (output_dir_path, alpha, beta)
         xticks = ["BF%d" % (_x) for _x in x_data ]
         x_label   = "beaufort".upper()
         y_label   = "probability".upper()
@@ -86,7 +86,7 @@ def all_kinds():
         matplotlib.pyplot.ylim(0, 0.40)
         matplotlib.pyplot.savefig(output_file_path)
         matplotlib.pyplot.clf()
-        result_key = "a_%d_b_%d" % (alpha, beta)
+        result_key = "a_%1.1lf_b_%1.1lf" % (alpha, beta)
         result[result_key] = {int(_d[0]): _d[1]for _d in bfs}
 
     output_file_path = "%s/result.json" % (output_dir_path)
