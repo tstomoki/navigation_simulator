adaa31d74c9f9f396e64721cdbffb94593e8feac
tstomoki <tstomoki4@gmail.com>
2015-12-03 16:04:15 +0900

    added constants

--- cui/scripts/main.py
+++ cui/scripts/main.py
@@ -248,7 +248,7 @@ def run(options):
         deltas   = np.linspace(BASE_DELTA['origin'], BASE_DELTA['end'], case_num)
 
         # for single case
-        trends = [0.05]
+        trends = [0.05, 0.10, 0.15]
         deltas = [0.30]
         for trend in trends:
             for delta in deltas:
